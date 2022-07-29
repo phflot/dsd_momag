35d8e74d7a5d4d6cda67c638e16d447dea02b27e
Philipp <PhilippFlotho@gmail.com>
2022-07-29 13:40:32 +0200

    refactor demo

--- Main.py
+++ Main.py
@@ -4,5 +4,5 @@ from os.path import join
 if __name__ == "__main__":
     MM = MotionMag(9, 0.1, 5, 0.1, 0.3, 4)
     # Put the base folder of CASME2-RAW here:
-    casme_folder = "D:\\OF_training\\CASME2-RAW"
-    MM.compute_micro_movement_mag(join(casme_folder, "sub14/EP04_04f.avi"), "./test_outout.avi")
+    casme_folder = "./CASME2-RAW"
+    MM.compute_micro_movement_mag(join(casme_folder, "sub14/EP04_04f.avi"), "./test_output.avi")
